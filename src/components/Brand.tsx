94265f35f0e042d62b58080f2e6f19a24d218fd5
RenanSouzadeOliveira <renansouzasccp@gmail.com>
2026-08-10 18:03:40 -0300

    update de saida gitpages

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 
 export function Brand({ compact = false }: { compact?: boolean }) {
   return (
     <span className={`brand ${compact ? "brand-compact" : ""}`}>
       <span className="brand-logo-frame">
-        <Image
+        <img
           className="brand-logo"
-          src="/logo.png"
+          src="logo.png"
           alt="Tax Innovation"
-          width={500}
-          height={500}
-          unoptimized
+          width="500"
+          height="500"
         />
       </span>
     </span>
